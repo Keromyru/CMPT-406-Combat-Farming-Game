1bed0efe184bf60ec22dc88c6ae3b6882a968d1e
mace239 <mab239@usask.ca>
2022-02-15 19:08:24 -0600

    Fixed issue with picking up 2D objects. Created some sample item information.

--- Assets/System/InputHandler/ItemGrabTester.cs
+++ Assets/System/InputHandler/ItemGrabTester.cs
@@ -33,7 +33,7 @@ public class ItemGrabTester : MonoBehaviour
     {
         Camera maincam = FindObjectOfType<Camera>();
 
-        // get the mouse's current position values and cast a ray from there
+        // get the mouse's current position values and cast a ray from there - 3D
         Ray ray = maincam.ScreenPointToRay(Mouse.current.position.ReadValue());
         RaycastHit hitItems;
 
@@ -45,6 +45,19 @@ public class ItemGrabTester : MonoBehaviour
             if (interactable != null) {
                 Debug.Log("Hit object " + interactable.name + " with right click");
                 interactable.Interact();
+            }
+        }
+
+        // get the mouse's current position values and cast a ray from there - 2D
+        // shoutout to this forum post: https://answers.unity.com/questions/1087239/get-2d-collider-with-3d-ray.html
+        RaycastHit2D hit2D = Physics2D.GetRayIntersection(ray); 
+
+        if (hit2D)
+        {
+            Interactable interactobj = hit2D.collider.GetComponent<Interactable>();
+            if (interactobj != null) {
+                Debug.Log("Hit 2D object " + interactobj.name);
+                interactobj.Interact();
             }
         }
     }
