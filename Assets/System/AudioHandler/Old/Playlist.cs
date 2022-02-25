d2673a663b07fd953a10e5f56a0d7ed58c214ca5
TK <tdk443@usask.ca>
2022-02-24 19:52:53 -0600

    Merged With Dev
    Merged With Current Dev and Verified it was still working

--- Assets/System/AudioHandler/Old/Playlist.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-[System.Serializable]
-public class Playlist {
-        public string name;
-        [Tooltip("All items with these tages will be added to a playlist when the named playlist is chosen ")]
-        public string musicTag;
-}
