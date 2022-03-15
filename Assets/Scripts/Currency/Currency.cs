743d6bdf78672ec574ca01fb6042be2f92be7711
bpw537 <bpw537@usask.ca>
2022-03-15 16:07:05 -0600

    Updated Currency
    As suggested I think I added a proper unity event for when the currency amount changes. Also added the set function for the currency amount.

--- Assets/Scripts/Currency/Currency.cs
+++ Assets/Scripts/Currency/Currency.cs
@@ -1,24 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 // A static class that holds the amount of money 
 public static class Currency
 {
     // The amount of money in inventory (Initial amount can be changed here)
     public static int money = 500;
+    // Unity Event for whenever money variable is changed
+    public static UnityEvent currencyEvent = new UnityEvent();
 
 
     // Add a given amount of money to the inventory
     public static void addMoney(int moneyToAdd)
     {
         money += moneyToAdd;
+        currencyEvent.Invoke();
     }
 
     // Add multiple amounts to the money counter (most likely used for selling seeds, items, etc)
     public static void addMultipleMoney(int moneyToAdd, int AmountofItemsSold)
     {
         money += (moneyToAdd * AmountofItemsSold);
+        currencyEvent.Invoke();
     }
 
     // Subtract a given amount of money from the inventory
@@ -33,11 +38,19 @@ public static class Currency
         {
             money -= moneyToSubtract;
         }
+        currencyEvent.Invoke();
     }
 
     // Gets the total amount of money currently in inventory (use this to get the amount instead of directly getting the money variable)
     public static int getMoney()
     {
         return money;
+    }
+
+    // Sets the total amount of money to the parameter given
+    public static void setMoney(int moneyToSetTo)
+    {
+        money = moneyToSetTo;
+        currencyEvent.Invoke();
     }
 }
