2843de8cb42456d59cd7fb299376943cb64dc4d2
Aurora Dubnyk <aurora@celtx.com>
2022-03-16 16:12:52 -0600

    Fixed the issue of plants not spawning in the correct place, currently only confirmed with hydra, can confirm later

--- Assets/System/InputHandler/GridClickListener.cs
+++ Assets/System/InputHandler/GridClickListener.cs
@@ -6,8 +6,10 @@ using UnityEngine.Tilemaps;
 // this is exprimental and not fully implemmented
 public class GridClickListener : MonoBehaviour
 {
-    [SerializeField]
-    private GridLayout gridHoldingTilemaps; 
+    [SerializeField] private GridLayout gridHoldingTilemaps; 
+
+    [SerializeField] private Tilemap tilemap;
+    
 
     public Camera cam;
     private Vector2 mousePosition;
@@ -19,9 +21,22 @@ public class GridClickListener : MonoBehaviour
 
     void Update(){
         if (Input.GetButtonDown("Fire1")){
-            mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
-            Vector3Int cellPosition = gridHoldingTilemaps.WorldToCell(mousePosition);
-            plantDatabase.spawnPlant("Hydra", new Vector2(cellPosition.x, cellPosition.y));
+            // Grab the mouse position
+            Vector3 mousePos = Input.mousePosition;
+            // create the ray for casting
+            Ray ray = cam.ScreenPointToRay(mousePos);
+            // essentially mimics the planes of the world and the obstacles (they are within the same plane space)
+            Plane plane = new Plane(Vector3.back, Vector3.zero);
+            float hitdist;
+            plane.Raycast(ray, out hitdist);
+            // get the point the ray intersected
+            var point = ray.GetPoint(hitdist);
+            // find the cell point
+            var cellPosition = tilemap.WorldToCell(point);
+            Vector3 centerPos = tilemap.GetCellCenterLocal(cellPosition);
+
+            // Creates the plant prefabs in the cell chosen by the player
+            GameObject plant = plantDatabase.spawnPlant("Hydra", new Vector2(centerPos.x, centerPos.y + (float) .18));
         }
     }
 }
