359349ca249e26c396c24764d2da6829f104964e
Melissa H <44488443+melishi@users.noreply.github.com>
2022-03-17 02:32:31 -0600

    Changed the Sorting layer of the obstcal map and Structure
    This is mostly cosmetic. But changed the sorting layers so the enemies talk nice to the thing.

--- Assets/Entities/Enemies/EnemySpawnTest.cs
+++ Assets/Entities/Enemies/EnemySpawnTest.cs
@@ -7,7 +7,7 @@ public class EnemySpawnTest : MonoBehaviour
     public EnemyDatabaseSO baddies;
 
     private void Awake() {
-        // baddies.spawnEnemy("Basic_enemy", new Vector2(-12.18753f,1.358912f));
+        baddies.spawnEnemy("Basic_enemy", new Vector2(-12.18753f,1.358912f));
         // baddies.spawnEnemy("Basic_enemy", new Vector2(-14.37506f,1.701823f));
     }
 }
