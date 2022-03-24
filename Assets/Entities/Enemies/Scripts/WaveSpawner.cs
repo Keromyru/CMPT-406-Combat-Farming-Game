b826a9d68d1c67125bb391f4e93dd007f8ac1a81
bpw537 <bpw537@usask.ca>
2022-03-23 22:23:44 -0600

    Added not finished wave spawner
    Enemies now spawn from 6 different points on the map. This is just from following a tutorial so still need to change it to work how we want it too.

---
+++ Assets/Entities/Enemies/Scripts/WaveSpawner.cs
@@ -0,0 +1,131 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Written by Blake following a Brackeys tutorial
+public class WaveSpawner : MonoBehaviour
+{
+    public enum SpawnState { SPAWNING, WAITING, COUNTING };
+
+    [System.Serializable]
+    public class Wave
+    {
+        public string name;
+        public Transform enemy;
+        public int count;
+        public float rate;
+    }
+
+    public Wave[] waves;
+    private int nextWave = 0;
+
+    public Transform[] spawnPoints;
+
+    // Will be replaced later for enemys spawning at night
+    public float timeBetweenWaves = 5f;
+    public float waveCountdown;
+
+    // Checks if there are enemies every second given (default is checks if there are enemies every 1 second)
+    // Basically to lower resource cost on computer
+    private float searchCountdown = 1f;
+
+    private SpawnState state = SpawnState.COUNTING;
+
+    private void Start()
+    {
+        if (spawnPoints.Length == 0)
+        {
+            Debug.LogError("No spawn points referenced");
+        }
+
+        waveCountdown = timeBetweenWaves;
+    }
+
+    private void Update()
+    {
+        if (state == SpawnState.WAITING)
+        {
+            // Check if enemies are still alive
+            if (!EnemyIsAlive())
+            {
+                // Begin new round/wave
+                WaveCompleted();
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        if (waveCountdown <= 0)
+        {
+            if (state != SpawnState.SPAWNING)
+            {
+                // Start spawning wave
+                StartCoroutine(SpawnWave(waves[nextWave]));
+            }
+        }
+        else
+        {
+            waveCountdown -= Time.deltaTime;
+        }
+    }
+
+    void WaveCompleted()
+    {
+        Debug.Log("wave completed!");
+
+        state = SpawnState.COUNTING;
+        waveCountdown = timeBetweenWaves;
+
+        if (nextWave + 1 > waves.Length - 1)
+        {
+            nextWave = 0;
+            Debug.Log("Completed all waves. Looping...");
+        }
+        else
+        {
+            nextWave++;
+        }
+    }
+
+    bool EnemyIsAlive()
+    {
+        searchCountdown -= Time.deltaTime;
+        if (searchCountdown <= 0f)
+        {
+            searchCountdown = 1f;
+            if (GameObject.FindGameObjectsWithTag("Enemy").Length == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    IEnumerator SpawnWave(Wave _wave)
+    {
+        Debug.Log("Spawning Wave: " + _wave.name);
+        state = SpawnState.SPAWNING;
+
+        // Spawn
+        for (int i = 0; i < _wave.count; i++)
+        {
+            SpawnEnemy(_wave.enemy);
+            yield return new WaitForSeconds(1f / _wave.rate);
+        }
+
+        state = SpawnState.WAITING;
+
+        yield break;
+    }
+
+    void SpawnEnemy(Transform _enemy)
+    {
+        // Spawn Enemy
+        Debug.Log("Spawning Enemy: " + _enemy.name);
+
+        Transform _sp = spawnPoints[Random.Range(0, spawnPoints.Length)];
+        Instantiate(_enemy, _sp.position, _sp.rotation);
+    }
+}
