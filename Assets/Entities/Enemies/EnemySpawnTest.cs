02844a24e22d359d3d72248673dbac00e9e72c10
Melissa H <44488443+melishi@users.noreply.github.com>
2022-03-23 21:21:32 -0600

    Added Social Distancing to Enmies Version 1,9
    As it says. added spacing to the enmies

--- Assets/Entities/Enemies/EnemySpawnTest.cs
+++ Assets/Entities/Enemies/EnemySpawnTest.cs
@@ -7,7 +7,17 @@ public class EnemySpawnTest : MonoBehaviour
     public EnemyDatabaseSO baddies;
 
     private void Awake() {
-        baddies.spawnEnemy("Basic_enemy", new Vector2(-5.18753f,1.358912f));
-      //  baddies.spawnEnemy("Basic_enemy", new Vector2(-4.37506f,1.701823f));
-    }
+        // // baddies.spawnEnemy("Basic_enemy", new Vector2(5.18753f,1.358912f));
+        // // baddies.spawnEnemy("Basic_enemy", new Vector2(4.37506f,1.701823f));
+        // baddies.spawnEnemy("Basic_enemy", new Vector2(-13.18753f,1.358912f));
+        // baddies.spawnEnemy("Basic_enemy", new Vector2(-11.37506f,1.701823f));
+        // // baddies.spawnEnemy("Basic_enemy", new Vector2(-5.18753f,1.358912f));
+        // // baddies.spawnEnemy("Basic_enemy", new Vector2(-4.37506f,1.701823f));
+    
+   for (int i = 0; i < 10; i++)
+        {
+            Vector2 offset =  Random.insideUnitCircle * 3;
+            baddies.spawnEnemy("Basic_enemy", new Vector2(-10+ offset.x ,1f + offset.y));
+        }
+}
 }
--- Assets/Entities/Enemies/Scripts/EnemyAI.cs
+++ Assets/Entities/Enemies/Scripts/EnemyAI.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using System.Linq; 
+
 public class EnemyAI : MonoBehaviour
 {
     private EnemyController myController;
@@ -10,6 +12,9 @@ public class EnemyAI : MonoBehaviour
     private GameObject myTarget; //Destination for this baddy
     private float enemyMoveSpeed;
     private GameObject theHub;
+    public List<GameObject> friendsList; //Dynamic List of Friendly Enemies
+    [SerializeField] float socialDistance = 1; //sets the distance of the friendly enmies that is acceptable  **which should obviously be 6m b/c yea
+    [SerializeField] bool tooClose = false;
 
     private void Start() {
         myController = this.GetComponent<EnemyController>(); //Quick Access to the controller
@@ -23,10 +28,42 @@ public class EnemyAI : MonoBehaviour
         //TODO: MAKE A THING THAT MAKES IT SO YOUR LIL DUDE DOESN"T WALK ALL UP IN IT'S LIL FRIENDS
         //Debug.Log(this.gameObject.name+" is attacking " + myTarget.name + " and it's "+ Distance() + " units away");
         //This Makes the Baddy Run Up To The Target
-        if (Distance() > myController.myEnemyData.attackRange - 0.2f){
+        if (Distance() > myController.myEnemyData.attackRange - 0.2f && !tooClose){
             Vector3 targetWithOffset = ((myTarget.transform.position - this.transform.position).normalized + myTarget.transform.position);
             myRB.MovePosition(Vector3.Lerp(this.transform.position, targetWithOffset , Time.deltaTime * enemyMoveSpeed * 0.25f));
         }
+       
+        foreach(GameObject friend in friendsList)
+        {
+            if(Vector2.Distance(this.transform.position,friend.transform.position) < socialDistance)
+            {
+                // knockback(friend.transform.position, 1);
+                // Debug.Log("to close friend");
+                
+                Vector2 direction = (transform.position - myTarget.transform.position).normalized;
+                // Debug.Log(friendDirection);
+                // Debug.Log(direction);
+                
+                
+                Vector2 friendDirection = (transform.position - myTarget.transform.position).normalized;
+                // Debug.Log(Vector2.Angle(myTarget.transform.position,friend.transform.position));
+                if(Vector2.Angle(myTarget.transform.position,friend.transform.position) < 10f)
+                {
+                    tooClose = true;
+
+                }
+                else{
+                    tooClose = false;
+                }
+            }
+            else{
+              tooClose = false;  
+            }
+        }
+        if(friendsList.Count()== 0){
+            tooClose = false;
+        }
+        
         
     }
     private void OnTriggerEnter2D(Collider2D entity) {
@@ -36,10 +73,12 @@ public class EnemyAI : MonoBehaviour
             targetList.Add(entity.gameObject); //Adds That Object From Its Attack List
             CheckTarget();
         }
-
-        
-        
+        else if (entity.tag == "Enemy") {
+            friendsList.Add(entity.gameObject); // Adds the friends to its list of friends 
+        }
     }
+
+
     private void OnTriggerExit2D(Collider2D entity) {
         if (entity.tag == "Plant" ||
             entity.tag == "Player" ||
@@ -48,6 +87,15 @@ public class EnemyAI : MonoBehaviour
             CheckTarget();
             if (targetList.Count == 0) { myTarget = theHub;} //Clears the target if there are not more options
         }
+        else if (entity.tag == "Enemy") {
+            friendsList.Remove(entity.gameObject); // Adds the friends to its list of friends 
+        }
+    }
+
+      public void knockback(Vector3 origin, float scale)
+    {   //Adds Momentum to the enemy in oposite direction of the Origin
+        Vector2 knockback = (transform.position - origin).normalized*scale;
+         GetComponent<Rigidbody2D>().AddForce(knockback,ForceMode2D.Impulse);
     }
 
     private void CheckTarget(){ //If the target doesn't exist, or it's out of range, or it's daytime;
