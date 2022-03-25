fd73e1956df0e67f2d685d1d60cca2d831e5e366
TK <tdk443@usask.ca>
2022-03-24 23:50:47 -0600

    Saving Before Branch Switch

--- Assets/Entities/Enemies/Scripts/StriderController.cs
+++ Assets/Entities/Enemies/Scripts/StriderController.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 public class StriderController : EnemyController
 {
-    private void Start() {
-        Debug.Log(myEnemyData.name);
-    }
     public override void onAttack(){
         base.onAttack();
     }
