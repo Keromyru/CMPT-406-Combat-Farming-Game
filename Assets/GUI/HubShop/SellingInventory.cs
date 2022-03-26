f81237c198042b2fda16dc832c43fa10a9aa3cac
mace239 <mab239@usask.ca>
2022-03-26 13:56:26 -0600

    FIXED SELLING. Needs collision detection to pop up shop & player currency to auto-update still

---
+++ Assets/GUI/HubShop/SellingInventory.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using System.Linq;
+
+// Mace
+
+// manages Inventory and Hotbar UI displays
+public class SellingInventory : MonoBehaviour
+{
+    Inventory inventory;  // get singleton inventory
+    public Transform itemsParent;  // the parent of all InventorySlots
+    InventorySlot[] slots;  // array of Inventory slots
+    public GameObject inventoryUI;  // reference to the actual GameObject
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // grab singleton & subscribe to changes in items
+        inventory = Inventory.instance;
+        inventory.onItemChangedCallback += UpdateUI;
+
+        // fill arrays with all the slots on scene
+        slots = itemsParent.GetComponentsInChildren<InventorySlot>();
+    }
+
+    // update UI for hotbar & inventory
+    void UpdateUI() {
+
+        //Debug.Log("Updating Inventory + Hotbar UI.");
+
+        // cycle through all slots, add item if one exists in our inventory
+        for (int i = 0; i < slots.Length; i++) {
+
+            KeyValuePair<Item, int> current_item;
+
+            if (i < inventory.items.Count) {
+                current_item = inventory.items.ElementAt(i);
+
+                slots[i].AddItem(current_item.Key, current_item.Value);
+
+            }
+            // if there isn't room, wipe the slot back to empty state
+            else {
+                slots[i].ClearSlot();
+            }
+        }
+    }
+}
