885e06e80d4624c507d661e11cad9b8c0b5bcd4b
Wilson <wiw394@mail.usask.ca>
2022-04-08 00:02:20 -0600

    Added End Game Scene.

--- Assets/GUI/EndScreen/script_EndgameController.cs
+++ Assets/GUI/EndScreen/script_EndgameController.cs
@@ -3,12 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class script_EndgameController : MonoBehaviour {
 	
 	// Store each display text to display stats
 	public TextMeshProUGUI days, monsters, money, score;
-
+	
+	public Button exitBtn;
+	
+	
+	void Start () {
+		
+		setStats();
+		
+		exitBtn.onClick.AddListener( ReturnToMenu );
+		
+	}
+	
+	public void ReturnToMenu() {
+		
+		GameStats.ResetAll();
+		SceneManager.LoadScene( "MainMenu", LoadSceneMode.Single );
+		
+	}
+		
     public void setStats() {
 		this.days.text = GameStats.getTotalDays().ToString();
 		this.monsters.text = GameStats.getTotalKills().ToString();
