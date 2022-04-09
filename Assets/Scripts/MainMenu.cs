998dc88344c7aef138154fa72aa23d98fe382408
Wilson <wiw394@mail.usask.ca>
2022-04-09 01:49:51 -0600

    Added exit button to main menu.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+	
+	[ Header( "Menu Buttons" ) ]
+	public Button exitButton;
+	
+    // Start is called before the first frame update
+    void Start() {
+		
+		myCursor.setDefault();
+		exitButton.onClick.AddListener( QuitGame );
+        
+    }
+	
+	public void QuitGame() {
+		
+		Debug.Log( "Closing Game" );
+		
+		Application.Quit();
+		
+	}
+
+}
